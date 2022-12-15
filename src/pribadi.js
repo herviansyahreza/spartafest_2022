6606744f347a10cf5431745278e2fcf5222eddcd
herviansyahreza <82496908+herviansyahreza@users.noreply.github.com>
2022-12-16 02:08:22 +0700

    Update pribadi.js

--- src/pribadi.js
+++ src/pribadi.js
@@ -20,7 +20,7 @@ const Perorangan = () => {
         });
 
         try {
-            const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/lombaindividu`, {
+            const response = await axios.post('https://backendprojectakhir-production-2d5b.up.railway.app/lombaindividu', {
                 bidang_lomba: data.get('lomba'),
                 nama: data.get('nama'),
                 nim: data.get('nim'),
