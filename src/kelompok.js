7bd603695c7977745bf9e65c58a3c0c960b5a172
herviansyahreza <reza.herviansyah99@gmail.com>
2022-12-16 14:49:45 +0700

    Update kelompok.js

--- src/kelompok.js
+++ src/kelompok.js
@@ -18,6 +18,50 @@ const Kelompok = () => {
             kontak: data.get('kontak'),
             alamat: data.get('alamat'),
             nama_satu: data.get('nama_satu'),
+            // nim_satu: data.get('nim_satu'),
+            // email_satu: data.get('email_satu'),
+            nama_dua: data.get('nama_dua'),
+            // nim_dua: data.get('nim_dua'),
+            // email_dua: data.get('email_dua'),
+        });
+
+        try {
+            const response = await axios.post(`https://backendspartafest-production.up.railway.app/lombakelompok`, {
+                bidang_lomba: data.get('lomba'),
+                nama_team: data.get('nama_team'),
+                nim: data.get('nim'),
+                ketua: data.get('ketua'),
+                email: data.get('email'),
+                asal_universitas: data.get('asal_universitas'),
+                kontak: data.get('kontak'),
+                alamat: data.get('alamat'),
+                nama_satu: data.get('nama_satu'),
+                // nim_satu: data.get('nim_satu'),
+                // email_satu: data.get('email_satu'),
+                nama_dua: data.get('nama_dua'),
+                // nim_dua: data.get('nim_dua'),
+                // email_dua: data.get('email_dua'),
+            });
+
+            // Navigate('/berhasil')
+        } catch (error) {
+            Navigate('/kelompok')
+            alert('Register Gagal');
+        }
+    };
+    const handleSubmitAnggota = async (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            bidang_lomba: data.get('lomba'),
+            nama_team: data.get('nama_team'),
+            nim: data.get('nim'),
+            ketua: data.get('ketua'),
+            email: data.get('email'),
+            asal_universitas: data.get('asal_universitas'),
+            kontak: data.get('kontak'),
+            alamat: data.get('alamat'),
+            nama_satu: data.get('nama_satu'),
             nim_satu: data.get('nim_satu'),
             email_satu: data.get('email_satu'),
             nama_dua: data.get('nama_dua'),
@@ -102,31 +146,30 @@ const Kelompok = () => {
 
                             <label className="text-left text-bold text-xl" for=""><b>Alamat</b></label><br />
                             <textarea className="w-full py-[15px] px-[10px] mb-[10px] bg-transparent border-b-[2px] text-black text-xl " name="alamat" placeholder="Alamat Lengkap" id="alamat" cols="20" rows="1" required></textarea><br />
-
-                            <h4 className="mt-[50px] mb-[20px] font-light text-3xl "><b>Data Anggota</b></h4>
-                            <p className="mt-[10px] mb-[60px] font-light  text-xl ">Jika Anggota berjumlah lebih dari 3 orang, cukup mengisi Form diatas</p>
-
-                            <label className="text-left text-bold text-xl" for=""><b>Nama Anggota 1</b></label><br />
-                            <input className="w-full py-[15px] px-[10px] mb-[10px] bg-transparent border-b-[2px] text-black text-xl " type="text" placeholder="Nama Anggota 1" name="nama_satu" id="nama_satu" /><br />
-
-                            <label className="text-left text-bold text-xl" for=""><b>Nomor Induk Mahasiswa</b></label><br />
-                            <input className="w-full py-[15px] px-[10px] mb-[10px] bg-transparent border-b-[2px] text-black text-xl " type="number" placeholder="NIM / KTM" name="nim_satu" id="nim_satu" /><br />
-
-                            <label className="text-left text-bold text-xl" for=""><b>Email</b></label><br />
-                            <input className="w-full py-[15px] px-[10px] mb-[10px] bg-transparent border-b-[2px] text-black text-xl " type="email" placeholder="Email Anggota 1" name="email_satu" id="email_satu" /><br />
-
-                            <label className="mt-[100px] text-left text-bold text-xl" for=""><b>Nama Anggota 2</b></label><br />
-                            <input className="w-full py-[15px] px-[10px] mb-[10px] bg-transparent border-b-[2px] text-black text-xl " type="text" placeholder="Nama Anggota 2" name="nama_dua" id="nama_dua" /><br />
-
-                            <label className="text-left text-bold text-xl" for=""><b>Nomor Induk Mahasiswa</b></label><br />
-                            <input className="w-full py-[15px] px-[10px] mb-[10px] bg-transparent border-b-[2px] text-black text-xl " type="number" placeholder="NIM / KTM" name="nim_dua" id="nim_dua" /><br />
-
-                            <label className="text-left text-bold text-xl" for=""><b>Email</b></label><br />
-                            <input className="w-full py-[15px] px-[10px] mb-[10px] bg-transparent border-b-[2px] text-black text-xl " type="email" placeholder="Email Anggota 2" name="email_dua" id="email_dua" /><br />
-
-
-                            <button class="button-33 bg-green-400 rounded-[100px] cursor-pointer py-[7px] px-[20px] inline-block text-center decoration-none touch-manipulation w-[130px] h-[50px] hover:bg-green-500 mt-[30px] " type="submit">Kirimkan</button>
+                            <button class="button-33 bg-green-400 rounded-[100px] cursor-pointer py-[7px] px-[20px] inline-block text-center decoration-none touch-manipulation w-[130px] h-[50px] hover:bg-green-500 mt-[30px] " type="submit">Kirimkan</button>    
                         </div>
+                    </form>
+                    <form onSubmit={handleSubmitAnggota}>
+                        <h4 className="mt-[50px] mb-[20px] font-light text-3xl "><b>Data Anggota</b></h4>
+                        <p className="mt-[10px] mb-[60px] font-light  text-xl ">Jika Anggota berjumlah lebih dari 3 orang, cukup mengisi Form diatas</p>
+
+                        <label className="text-left text-bold text-xl" for=""><b>Nama Anggota 1</b></label><br />
+                        <input className="w-full py-[15px] px-[10px] mb-[10px] bg-transparent border-b-[2px] text-black text-xl " type="text" placeholder="Nama Anggota 1" name="nama_satu" id="nama_satu" /><br />
+                        <label className="text-left text-bold text-xl" for=""><b>Nomor Induk Mahasiswa</b></label><br />
+                        <input className="w-full py-[15px] px-[10px] mb-[10px] bg-transparent border-b-[2px] text-black text-xl " type="number" placeholder="NIM / KTM" name="nim_satu" id="nim_satu" /><br />
+                        <label className="text-left text-bold text-xl" for=""><b>Email</b></label><br />
+                        <input className="w-full py-[15px] px-[10px] mb-[10px] bg-transparent border-b-[2px] text-black text-xl " type="email" placeholder="Email Anggota 1" name="email_satu" id="email_satu" /><br />
+
+                        <label className="mt-[100px] text-left text-bold text-xl" for=""><b>Nama Anggota 2</b></label><br />
+                        <input className="w-full py-[15px] px-[10px] mb-[10px] bg-transparent border-b-[2px] text-black text-xl " type="text" placeholder="Nama Anggota 2" name="nama_dua" id="nama_dua" /><br />
+
+                        <label className="text-left text-bold text-xl" for=""><b>Nomor Induk Mahasiswa</b></label><br />
+                        <input className="w-full py-[15px] px-[10px] mb-[10px] bg-transparent border-b-[2px] text-black text-xl " type="number" placeholder="NIM / KTM" name="nim_dua" id="nim_dua" /><br />
+
+                        <label className="text-left text-bold text-xl" for=""><b>Email</b></label><br />
+                        <input className="w-full py-[15px] px-[10px] mb-[10px] bg-transparent border-b-[2px] text-black text-xl " type="email" placeholder="Email Anggota 2" name="email_dua" id="email_dua" /><br />
+                        <button class="button-33 bg-green-400 rounded-[100px] cursor-pointer py-[7px] px-[20px] inline-block text-center decoration-none touch-manipulation w-[130px] h-[50px] hover:bg-green-500 mt-[30px] " type="submit">Kirimkan</button>
+
                     </form>
                     
                 </section>
